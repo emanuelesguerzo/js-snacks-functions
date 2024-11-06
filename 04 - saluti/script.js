2c77b30517866dd6351148c2add051d67f635da4
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-06 15:48:18 +0100

    feat: #4 bonus exercise complete

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -14,3 +14,24 @@ const greeting = (name) => `ciao ${name}`;
 console.log(greeting("Mario"));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
+
+//BONUS 
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+const name2 = 'Mario';
+
+// Dichiara la funzione qui.
+/**
+ * Description
+ * @param {string} name
+ * @returns {string}
+ */
+
+function greeting2(name) {
+    const greet = "ciao " + name;
+    return greet;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+console.log(greeting2("Mario"));
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
