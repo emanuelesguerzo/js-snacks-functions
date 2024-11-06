88183970d0386ea871f53a797782a3b9701e06d4
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-06 13:52:24 +0100

    feat: 'iniziali' exercise complete

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,13 +1,16 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
-
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+/**
+ * Description
+ * @param {array} array
+ * @returns {array}
+ */
 
+const getInitials = (array) => array.map(firstLetter => firstLetter[0]);
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(getInitials(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
