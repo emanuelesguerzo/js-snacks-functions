3fc9ce35eff52b36ee0f8c87589d7bca7dfd38a9
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-06 15:44:19 +0100

    feat: #3 bonus exercise complete

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -18,3 +18,27 @@ const getVowels = (string) => {
 console.log(getVowels(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+//BONUS
+/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+const word2 = 'javascript';
+
+// Dichiara la funzione qui.
+/**
+ * Description
+ * @param {string} string
+ * @returns {string}
+ */
+
+function getVowels2(string) {
+    const vowels2 = ["a", "e", "i", "o", "u"];
+    const foundVowels = string.split('').filter(function(letter) {
+        return vowels2.includes(letter);
+    });
+    return `${foundVowels.length} (${foundVowels.join(", ")})`;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+console.log(getVowels2(word2));
+
+//Risultato atteso se si passa 'javascript': 3 (a, a, i)
