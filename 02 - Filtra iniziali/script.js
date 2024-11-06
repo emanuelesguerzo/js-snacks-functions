278f27f587947206252f0da8bbcfd8abccfd66a9
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-06 14:06:51 +0100

    feat: 'filtra iniziali' exercise complete

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -1,13 +1,10 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
-
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+const getWordByLetter = (array, letter) => array.filter(word => word[0] === letter);
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(getWordByLetter(names, "A"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
